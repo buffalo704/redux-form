bd0ef7e268018c813a3c9aa284bd7a875fab430f
Sid <buffalo704@hotmail.com>
2017-06-23 00:09:02 -0400

    form validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -22,18 +22,18 @@ class PostsNew extends Component {
         return (
             <form>
                 <Field
-                    label="Title"
+                    label="Title For Post"
                     name="title"
                     component={ this.renderField }
                 />
                 <Field
-                    label="Tags"
-                    name="tags"
+                    label="Categories"
+                    name="categories"
                     component={ this.renderField }
                 />
                 <Field
                     label="Post Content"
-                    name="cat"
+                    name="content"
                     component={ this.renderField }
                 />
             </form>
@@ -41,6 +41,28 @@ class PostsNew extends Component {
     }
 }
 
+function validate (values) {
+    const errors = {};
+
+    // Validate the input from 'values'
+    if (!values.title || values.title.length < 3) {
+        errors.title = "Enter a title!";
+    }
+
+    if (!values.categories) {
+        errors.categories = "Enter some categories!";
+    }
+
+    if (!values.content) {
+        errors.content = "Enter some content!";
+    }
+
+    // If errors is empty, the form is fine to submit
+    // If errors has *any* properties, redux form assumes is invalid
+    return errors;
+}
+
 export default reduxForm({
+    validate,
     form: 'PostsNewForm'
 }) (PostsNew);
